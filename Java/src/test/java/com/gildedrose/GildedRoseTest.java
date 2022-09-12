a31750537dcccf7359048a8de9fd552ae8c9cbf7
Hassan Elseoudy <hassan.elseoudy@zalando.de>
2022-09-12 11:08:32 +0200

    :recycle: Convert gGildedRose to kotlin.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,10 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].getName());
+        assertEquals("foo", app.getItems()[0].getName());
     }
 
 }
