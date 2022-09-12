70b98d5d39a102d1b33b2af024057dadcbaf26e4
Hassan Elseoudy <hassan.elseoudy@zalando.de>
2022-09-12 14:12:28 +0200

    :recycle: Conjured.

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -6,16 +6,35 @@ open class Item(var name: String, var sellIn: Int = 0, var quality: Int = 0) {
     override fun toString(): String = "$name, $sellIn, $quality"
 }
 
+object Aging {
+    val standard: () -> Int = { 1 }
+    val none: () -> Int = { 1 }
+
+}
+
+object Degradation {
+    val standard: (Int, Int) -> Int = { sellIn: Int, _: Int -> if (sellIn < 0) 2 else 1 }
+    val none: (Int, Int) -> Int = { _, _ -> 0 }
+
+}
+
+object Saturation {
+    val standard: (Int) -> Int = fun(quality: Int): Int = when {
+        quality < 0 -> 0
+        quality > 50 -> 50
+        else -> quality
+    }
+    val none: (Int) -> Int = { it }
+
+}
+
+
 class BaseItem(name: String,
                sellIn: Int = 0,
                quality: Int = 0,
-               private val aging: () -> Int = { 1 },
-               private val degradation: (Int, Int) -> Int = { sellIn: Int, _: Int -> if (sellIn < 0) 2 else 1 },
-               private val saturation: (Int) -> Int = fun(quality: Int): Int = when {
-                   quality < 0 -> 0
-                   quality > 50 -> 50
-                   else -> quality
-               })
+               private val aging: () -> Int = Aging.standard,
+               private val degradation: (Int, Int) -> Int = Degradation.standard,
+               private val saturation: (Int) -> Int = Saturation.standard)
     : Item(name, sellIn, quality) {
     open fun update() {
         sellIn -= aging()
@@ -29,9 +48,9 @@ fun Sulfuras(name: String, sellIn: Int = 0, quality: Int = 0) = BaseItem(
     name,
     sellIn,
     quality,
-    aging = { 0 },
-    degradation = { _, _ -> 0 },
-    saturation = { it })
+    aging = Aging.none,
+    degradation = Degradation.none,
+    saturation = Saturation.none)
 
 
 fun Brie(name: String, sellIn: Int = 0, quality: Int = 0) = BaseItem(
@@ -51,5 +70,16 @@ fun Pass(name: String, sellIn: Int = 0, quality: Int = 0) = BaseItem(
             sellIn < 5 -> -3
             sellIn < 10 -> -2
             else -> -1
+        }
+    })
+
+fun Conjured(name: String, sellIn: Int = 0, quality: Int = 0) = BaseItem(
+    name,
+    sellIn,
+    quality,
+    degradation = { sellIn: Int, quality: Int ->
+        when {
+            sellIn < 0 -> 4
+            else -> 2
         }
     })
