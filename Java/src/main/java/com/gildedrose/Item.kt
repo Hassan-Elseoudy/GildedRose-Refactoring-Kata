87bb8e5f14d7c8e9d0b614c8bfa83a18e7c4ffd3
Hassan Elseoudy <hassan.elseoudy@zalando.de>
2022-09-12 11:49:29 +0200

    :fire: Remove update2 after merging.

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -7,12 +7,17 @@ open class Item(var name: String, var sellIn: Int = 0, var quality: Int = 0) {
 open class BaseItem(name: String, sellIn: Int = 0, quality: Int = 0) : Item(name, sellIn, quality) {
     open fun update() {
         val name = name
-        update1()
         age()
-        update2()
+        degrade()
     }
 
-    protected open fun update2() {
+    protected open fun age() {
+        sellIn -= 1
+    }
+
+    protected open fun degrade() {
+        if (quality > 0)
+            quality -= 1
         if (sellIn < 0) {
             if (quality > 0) {
                 quality -= 1
@@ -20,31 +25,13 @@ open class BaseItem(name: String, sellIn: Int = 0, quality: Int = 0) : Item(name
         }
     }
 
-    protected open fun age() {
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn -= 1
-        }
-    }
-
-    protected open fun update1() {
-        if (quality > 0)
-            quality -= 1
-    }
-
 }
 
 class Brie(name: String, sellIn: Int = 0, quality: Int = 0) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
         if (quality < 50) {
             quality += 1
         }
-    }
-
-    override fun age() {
-        sellIn -= 1
-    }
-
-    override fun update2() {
         if (sellIn < 0) {
             if (quality < 50) {
                 quality += 1
@@ -52,47 +39,45 @@ class Brie(name: String, sellIn: Int = 0, quality: Int = 0) : BaseItem(name, sel
         }
     }
 
+    override fun age() {
+        sellIn -= 1
+    }
+
 
 }
 
 class Sulfuras(name: String, sellIn: Int = 0, quality: Int = 0) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
     }
 
     override fun age() {
     }
-
-    override fun update2() {}
-
 
 }
 
 
 class Pass(name: String, sellIn: Int = 0, quality: Int = 0) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
         if (quality < 50) {
             quality += 1
-            if (sellIn < 11) {
+            if (sellIn < 10) {
                 if (quality < 50) {
                     quality += 1
                 }
             }
-            if (sellIn < 6) {
+            if (sellIn < 5) {
                 if (quality < 50) {
                     quality += 1
                 }
             }
+        }
+        if (sellIn < 0) {
+            quality = 0
         }
     }
 
     override fun age() {
         sellIn -= 1
-    }
-
-    override fun update2() {
-        if (sellIn < 0) {
-            quality -= quality
-        }
     }
 
 
