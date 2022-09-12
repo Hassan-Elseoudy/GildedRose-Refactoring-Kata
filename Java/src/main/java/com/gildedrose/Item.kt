cc8fc4a05b68c559dc0dc53bee715d90bd56765e
Hassan Elseoudy <hassan.elseoudy@zalando.de>
2022-09-12 12:04:56 +0200

    :recycle: More simplification for Sulfuras.

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -9,6 +9,12 @@ open class BaseItem(name: String, sellIn: Int = 0, quality: Int = 0) : Item(name
         val name = name
         age()
         degrade()
+        saturate()
+    }
+
+    protected open fun saturate() {
+        if (quality < 0) quality = 0
+        if (quality > 50) quality = 50
     }
 
     protected open fun age() {
@@ -16,68 +22,59 @@ open class BaseItem(name: String, sellIn: Int = 0, quality: Int = 0) : Item(name
     }
 
     protected open fun degrade() {
-        if (quality > 0)
-            quality -= 1
-        if (sellIn < 0) {
-            if (quality > 0) {
-                quality -= 1
-            }
-        }
+        quality -= if (sellIn < 0) 2 else 1
     }
 
 }
 
 class Brie(name: String, sellIn: Int = 0, quality: Int = 0) : BaseItem(name, sellIn, quality) {
     override fun degrade() {
-        if (quality < 50) {
-            quality += 1
-        }
-        if (sellIn < 0) {
-            if (quality < 50) {
-                quality += 1
-            }
-        }
+        quality += if (sellIn < 0) 2 else 1
     }
 
     override fun age() {
         sellIn -= 1
+    }
+
+    override fun saturate() {
+        when {
+            quality < 0 -> quality = 0
+            quality > 50 -> quality = 50
+        }
     }
 
 
 }
 
 class Sulfuras(name: String, sellIn: Int = 0, quality: Int = 0) : BaseItem(name, sellIn, quality) {
-    override fun degrade() {
-    }
+    override fun degrade() {}
 
-    override fun age() {
-    }
+    override fun age() {}
+
+    override fun saturate() {}
 
 }
 
 
 class Pass(name: String, sellIn: Int = 0, quality: Int = 0) : BaseItem(name, sellIn, quality) {
     override fun degrade() {
-        if (quality < 50) {
-            quality += 1
-            if (sellIn < 10) {
-                if (quality < 50) {
-                    quality += 1
-                }
-            }
-            if (sellIn < 5) {
-                if (quality < 50) {
-                    quality += 1
-                }
-            }
+        quality = when {
+            sellIn < 0 -> 0
+            sellIn < 5 -> quality + 3
+            sellIn < 10 -> quality + 2
+            else -> quality + 1
+
         }
-        if (sellIn < 0) {
-            quality = 0
-        }
+
     }
 
     override fun age() {
         sellIn -= 1
+    }
+
+    override fun saturate() {
+        if (quality < 0) quality = 0
+        if (quality > 50) quality = 50
     }
 
 
